99e790d9e4d8cd218ba45c90fd1283f8eab5c495
Ellen Duboc <ellenduboc-fsa@hotmail.com>
2020-05-19 11:23:21 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './config/ReactotronConfig';
+
+import Routes from './routes';
+import Main from './pages/Main';
+
+export default function App() {
+    return (
+        <Routes />
+    );
+};
+
+Main.navigationOptions = {
+    title: 'Usuários',
+};
