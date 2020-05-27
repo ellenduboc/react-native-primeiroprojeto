9ae5e7981b2861d20189ff951b967c193dab0cd2
Ellen Duboc <ellenduboc-fsa@hotmail.com>
2020-05-27 15:18:31 -0300

    Adds pull to refresh and infinity scroll

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { ActivityIndicator, View, StyleSheet } from 'react-native';
+
 import PropTypes from 'prop-types';
 
 import { PanResponder } from 'react-native';
@@ -20,6 +22,8 @@ import {
 
 import api from '../../services/api';
 
+const perPage = 20;
+
 export default class User extends Component {
     static navigationOptions = ({ navigation }) => ({
         title: navigation.getParam('user').name,
@@ -32,45 +36,116 @@ export default class User extends Component {
     };
 
     state = {
+        page: 1,
+        loading: false,
         stars: [],
+        screenLoading: true,
+        refreshing: false,
+        setRefreshing: false,
+
     };
 
     async componentDidMount() {
         const { navigation } = this.props;
+        const { page } = this.state;
         const user = navigation.getParam('user');
+
+        const response = await api.get(`/users/${user.login}/starred?per_page=${perPage}&page=${this.state.page}`);
+
+
+        this.setState({
+            stars: response.data, screenLoading: false, page: page + 1,
+        });
+    }
+
+
+    loadRepositories = async (shouldRefresh = false) => {
+        if (this.state.loading) return;
+
+        const { navigation } = this.props;
+
+        const user = navigation.getParam('user');
+
+        const { page } = this.state;
+
+        this.setState({ loading: true });
 
         const response = await api.get(`/users/${user.login}/starred`);
 
-        this.setState({ stars: response.data });
+        this.setState({
+            stars: (shouldRefresh ? response.data : [...this.state.stars, ...response.data]),
+            page: page + 1,
+            loading: false,
+        });
+    };
+
+    refreshList = () => {
+        this.setState({ setRefreshing: true });
+
+        this.loadRepositories(this.state.setRefreshing);
+
+        this.setState({ setRefreshing: false });
     }
+
+    renderFooter = () => {
+        if (!this.state.loading) return null;
+        return (
+            <View style={styles.loading}>
+                <ActivityIndicator />
+            </View>
+        );
+    };
+
     render() {
         const { navigation } = this.props;
 
-        const { stars } = this.state;
+        const { stars, screenLoading } = this.state;
 
         const user = navigation.getParam('user');
 
+
+
         return (
+
             <Container>
-                <Header>
-                    <Avatar source={{ uri: user.avatar }} />
-                    <Name>{user.name}</Name>
-                    <Bio>{user.bio}</Bio>
-                </Header>
-                <Stars
-                    data={stars}
-                    keyExtractor={(star) => String(star.id)}
-                    renderItem={({ item }) => (
-                        <Starred>
-                            <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
-                            <Info>
-                                <Title>{item.name}</Title>
-                                <Author>{item.owner.login}</Author>
-                            </Info>
-                        </Starred>
+                {screenLoading ? (
+                    <ActivityIndicator size="large" color="gray" />
+                ) : (
+                        <View style={{ padding: 30 }}>
+                            <Header>
+                                <Avatar source={{ uri: user.avatar }} />
+                                <Name>{user.name}</Name>
+                                <Bio>{user.bio}</Bio>
+                            </Header>
+                            <Stars
+                                onEndReached={this.loadRepositories}
+                                onEndReachedThreshold={0.2}
+                                onRefresh={this.refreshList}
+                                refreshing={this.state.refreshing}
+                                ListFooterComponent={this.renderFooter}
+                                data={stars}
+                                keyExtractor={(star) => String(star.id)}
+                                renderItem={({ item }) => (
+                                    <Starred>
+                                        <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                                        <Info>
+                                            <Title>{item.name}</Title>
+                                            <Author>{item.owner.login}</Author>
+                                        </Info>
+
+                                    </Starred>
+                                )}
+                            />
+                        </View>
                     )}
-                />
             </Container>
         );
     };
 };
+
+const styles = StyleSheet.create({
+    loading: {
+        alignSelf: 'center',
+        marginVertical: 20,
+    },
+});
